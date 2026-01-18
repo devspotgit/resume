0e974df3748b8917f81d4b2c6217303a011f6a1c
Patrick <jpatricktadie@gmail.com>
2026-01-17 20:41:02 -0500

    lines added

--- src/templates.js
+++ src/templates.js
@@ -6,13 +6,13 @@ function side(personalInfo, skills){
     return `
         <div class="side">
             <div class="side-header">
-                <span>${personalInfo.name}</span>
-                <span>${personalInfo.title}</span>
+                <span class="name">${personalInfo.name}</span>
+                <span class="title">${personalInfo.title}</span>
             </div>
             <div class="side-body">
-                <div class="contact">
-                    <span>Contact</span>
-                    <div class="contact-list">
+                <div class="side-section">
+                    <span class="side-section-title">Contact</span>
+                    <div class="side-section-content">
                         <div class="contact-item">
                             <span><i class="fa-solid fa-envelope"></i></span>
                             <span>${personalInfo.email}</span>
@@ -23,24 +23,24 @@ function side(personalInfo, skills){
                         </div>
                         <div class="contact-item">
                             <span><i class="fa-brands fa-linkedin-in"></i></span>
-                            <a href="${personalInfo.linkedin}">${personalInfo.linkedin}</a>
+                            <span>${personalInfo.linkedin}</span>
                         </div>
                         <div class="contact-item">
                             <span><i class="fa-brands fa-github"></i></span>
-                            <a href="${personalInfo.github}">${personalInfo.github}</a>
+                            <span>${personalInfo.github}</span>
                         </div>
                         <div class="contact-item">
                             <span><i class="fa-solid fa-briefcase"></i></span>
-                            <a href="${personalInfo.portfolio}">${personalInfo.portfolio}</a>
+                            <span>${personalInfo.portfolio}</span>
                         </div>
                     </div>
                 </div>
-                <div class="skills">
-                    <span>Skills</span>
-                    <div class="skill-list">
+                <div class="side-section">
+                    <span class="side-section-title">Skills</span>
+                    <div class="side-section-content">
                         ${
                             skills.map(skill => `
-                                <span>${skill}</span>
+                                <span class="skill-item">${skill}</span>
                             `).join(" ")
                         }
                     </div>
@@ -54,23 +54,23 @@ function main(summary, education, experience, project){
 
     return `
         <div class="main">
-            <div class="summary">
-                <span>Summary</span>
+            <div class="main-section summary">
+                <span class="main-section-title">Summary</span>
                 <p>${summary}</p>
             </div>
 
-            <div class="education">
-                <span>Education</span>
-                <div class="education-list">
+            <div class="main-section">
+                <span class="main-section-title">Education</span>
+                <div class="main-section-content">
                     ${
                         education.map(educ => `
                             <div class="education-item">
-                                <span>${educ.startDate+" - "+educ.endDate}</span>
                                 <span>${educ.program}</span>
                                 <span>${educ.school}</span>
+                                <span>${educ.startDate+" - "+educ.endDate}</span>
                                 <ul class="education-item-desc">
                                     ${
-                                        educ.description.map(item => `
+                                        educ.description.slice(0, 4).map(item => `
                                             <li>${item}</li>
                                         `).join(" ")
                                     }
@@ -81,15 +81,15 @@ function main(summary, education, experience, project){
                 </div>
             </div>
 
-            <div class="experience">
-                <span>Experience</span>
-                <div class="experience-list">
+            <div class="main-section">
+                <span class="main-section-title">Experience</span>
+                <div class="main-section-content">
                     ${
                         experience.map(exp => `
-                            <div class="education-item">
-                                <span>${exp.startDate+" - "+exp.endDate}</span>
+                            <div class="experience-item">
                                 <span>${exp.position}</span>
                                 <span>${exp.company}</span>
+                                <span>${exp.startDate+" - "+exp.endDate}</span>
                                 <ul class="experience-item-desc">
                                     ${
                                         exp.description.map(item => `
@@ -103,9 +103,9 @@ function main(summary, education, experience, project){
                 </div>
             </div>
 
-            <div class="project">
-                <span>Recent Projects</span>
-                <div class="project-list">
+            <div class="main-section">
+                <span class="main-section-title">Recent Project</span>
+                <div class="main-section-content">
                     ${
                         project.map(item => `
                             <div class="project-item">
